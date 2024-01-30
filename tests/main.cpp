c89bcbdaa8d2913105ddbf105ca1f8f0e3e17fde
Konstantin Gevorkyan <konstantin.gevorkyan@shazzle.com>
2024-01-30 23:18:39 +0500

    Add tests for upload\download filepaths generation

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,13 +2,47 @@
  *  ᛝ
  */
 
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-#include <iostream> // @todo delete this
+#include <filesystem>
 
-TEST_CASE("")
+#include <boost/asio/io_context.hpp>
+
+#include "http_client.hpp"
+#include "stream_crc32.hpp"
+#include "utils/filepaths.hpp"
+
+//boost::asio::io_context io_context;
+//auto settings = tt_tests::make_connect({}, {}, {}, {}, {});
+
+//tt_tests::http_client client( io_context, [](std::string, std::uint32_t) {} );
+//tt_tests::upload_filepaths
+//tt_tests::download_filepaths
+
+TEST_CASE("prepare_files")
 {
-	CHECK(true);
-	CHECK(false);
+	REQUIRE_FALSE(tt_tests::upload_resource_dir.empty());
+	REQUIRE_FALSE(tt_tests::download_resourse_dir.empty());
+
+	REQUIRE_NOTHROW(tt_tests::throw_if_no_exist(tt_tests::upload_resource_dir));
+	REQUIRE_NOTHROW(tt_tests::throw_if_no_exist(tt_tests::download_resourse_dir));
+
+	REQUIRE_NOTHROW(tt_tests::throw_if_no_directory(tt_tests::upload_resource_dir));
+	REQUIRE_NOTHROW(tt_tests::throw_if_no_directory(tt_tests::download_resourse_dir));
+	
+	REQUIRE_NOTHROW(tt_tests::generate_upload_filepaths(tt_tests::upload_resource_dir));
+	REQUIRE_NOTHROW(tt_tests::generate_download_filepaths(tt_tests::download_resourse_dir));
 }
+
+
+TEST_CASE("single client session")
+{
+	boost::asio::io_context io_context;
+
+	auto settings = tt_tests::make_connect({}, {}, {}, {}, {});
+
+	tt_tests::http_client client( io_context, [](std::string, std::uint32_t) {} );
+}
+
