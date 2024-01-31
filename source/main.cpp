754be773a81078741d66a7169a69b3e41dadea73
Konstantin Gevorkyan <konstantin.gevorkyan@shazzle.com>
2024-02-01 03:43:13 +0500

    Add simple async ssl server
    It's based by boost beast http_ssl_async_server example

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,41 @@
  *  ᛝ
  */
 
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
 
-int main(int argc, char **argv)
+#include "error/error.hpp"
+#include "http_server.hpp"
+#include "settings.hpp"
+
+
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello world" << std::endl;
+    std::int32_t errc = error::to_int(error::errc::ok);
 
-	return 0;
+    try
+    {
+        tt_program::http_server server;
+
+        server.run(tt_program::make_settings(argc, argv));
+    }
+    catch(const std::logic_error & e)
+    {
+        std::cerr << e.what() << std::endl;
+        errc = error::to_int(error::errc::invalid_argument);
+    }
+    catch(...)
+    {
+        std::cerr << "Unknow Error" << std::endl;
+        errc = error::to_int(error::errc::unknown);
+    }
+
+    return errc;
 }
