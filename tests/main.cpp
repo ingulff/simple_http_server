e49957741be7b3b8f5740e82fdc1560e13f637ce
Konstantin Gevorkyan <konstantin.gevorkyan@shazzle.com>
2024-02-01 03:45:26 +0500

    Add connect tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,14 +7,19 @@
 #include <doctest/doctest.h>
 
 #include <filesystem>
+#include <system_error>
+#include <type_traits>
 
 #include <boost/asio/io_context.hpp>
 
+#include "error/http_error.hpp"
+#include "handle_response.hpp"
 #include "http_client.hpp"
 #include "utils/filepaths.hpp"
 #include "utils/filesystem_helpers.hpp"
-#include "utils/run_io_context.hpp"
-#include "utils/stream_crc32.hpp"
+
+#include <memory>
+#include "http_session.hpp"
 
 //boost::asio::io_context io_context;
 //auto settings = tt_tests::make_connect({}, {}, {}, {}, {});
@@ -23,16 +28,15 @@
 //tt_tests::upload_filepaths
 //tt_tests::download_filepaths
 
+
 TEST_CASE("prepare_files")
 {
 	REQUIRE_FALSE(tt_tests::upload_resource_dir.empty());
 	REQUIRE_FALSE(tt_tests::download_resourse_dir.empty());
 
 	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_exist(tt_tests::upload_resource_dir));
-	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_exist(tt_tests::download_resourse_dir));
 
 	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_directory(tt_tests::upload_resource_dir));
-	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_directory(tt_tests::download_resourse_dir));
 	
 	REQUIRE_NOTHROW(tt_tests::generate_upload_filepaths(tt_tests::upload_resource_dir));
 	REQUIRE_NOTHROW(tt_tests::generate_download_filepaths(tt_tests::download_resourse_dir));
@@ -43,19 +47,72 @@ TEST_CASE("prepare_files")
 }
 
 
-TEST_CASE("single client session")
+TEST_CASE("single_client_session")
 {
 	boost::asio::io_context io_context;
 
-	auto settings = tt_tests::make_connect({}, {}, {}, {}, {});
+	tt_tests::http_client client( io_context, { handle_response_body } );
+#if 0  // есть некоторый конфликт в сертификатах? boost-дефолтные принимает только boost::server
+	SUBCASE("google.com_without_hhtps")
+	{
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::moved_permanently;
+		auto settings = tt_tests::make_connect("/index.html", "google.com", 443, 11, tt_tests::http_client::http_method::GET);
+		client.send_https( settings );
 
-	tt_tests::http_client client( io_context, [](std::string, std::uint32_t) {} );
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
 
-	tt_program::utils::run_io_context(io_context);
+	SUBCASE("google.com_invalid_filepath")
+	{
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::not_found;
+		auto settings = tt_tests::make_connect("/test.txt", "google.com", 443, 11, tt_tests::http_client::http_method::GET);
+		client.send_http( settings );
 
-	//client.send_https();
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
+#endif
+	SUBCASE("single_request_to_http_server_file_no_exist")
+	{
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::not_found;
+		auto settings = tt_tests::make_connect("/test__qqwerty.txt", "127.0.0.1", 1234, 11, tt_tests::http_client::http_method::GET);
 
+		client.send_http( settings );
 
-	
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
+#if 0
+	SUBCASE("single_request_to_http_server_exist_file")
+	{
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::not_found;
+		auto settings = tt_tests::make_connect("/resource/uploadtest_.txt", "127.0.0.1", 1234, 11, tt_tests::http_client::http_method::GET);
+
+		client.send_https( settings );
+
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
+#endif
 }
 
+
+TEST_CASE("clear_section")
+{
+	REQUIRE_FALSE(tt_tests::download_resourse_dir.empty());
+
+	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_exist(tt_tests::download_resourse_dir));
+
+	REQUIRE_NOTHROW(tt_tests::generate_download_filepaths(tt_tests::download_resourse_dir));
+
+	//REQUIRE(tt_tests::clear_downloading_directory() == std::error_code{});
+}
