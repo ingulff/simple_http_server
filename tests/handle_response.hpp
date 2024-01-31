14967f16f3af2b8efde923fa106168b3a804e80b
Konstantin Gevorkyan <konstantin.gevorkyan@shazzle.com>
2024-01-31 13:28:00 +0500

    Rename response handlers and move to main tests dir

--- tests/handle_response.hpp
+++ tests/handle_response.hpp
@@ -1,31 +1,39 @@
-#ifndef TT_HANDLE_RESPONSE_POLICY_HPP
-#define TT_HANDLE_RESPONSE_POLICY_HPP
+#ifndef TT_HANDLE_RESPONSE_HPP
+#define TT_HANDLE_RESPONSE_HPP
+
+#include <sstream>
 
 #include "error/http_error.hpp"
+#include "utils/filepaths.hpp"
 #include "utils/stream_crc32.hpp"
 
 namespace tt_tests::utils
 {
 
 inline error::http_errc expected_errc;
+inline bool is_valid_file = false;
+inline bool is_reseived = false;
 
 
 } // tt_tests::utils
 
+#include <iostream>
 void handle_response_body(std::string response, std::uint32_t errc)
 {
     std::istringstream stream{std::move(response)};
         
     auto handler = tt_tests::make_stream_crc32();
     auto hash = handler->calc_crc32(stream);
-
-    CHECK(error::http_errc::sucesss == error::to_http_errc(errc));
-    CHECK(true == tt_tests::upload_files_hashs.count(hash));
+std::cout << "on receive " << std::endl;
+    CHECK(tt_tests::utils::expected_errc == error::to_http_errc(errc));
+    CHECK(tt_tests::utils::is_valid_file == tt_tests::upload_files_hashs.count(hash));
+    tt_tests::utils::is_reseived = true;
 }
 
 void handle_response_error(std::string, std::uint32_t errc)
 {
     REQUIRE(tt_tests::utils::expected_errc == error::to_http_errc(errc));
+    tt_tests::utils::is_reseived = true;
 }
 
-#endif // TT_HANDLE_RESPONSE_POLICY_HPP
+#endif // TT_HANDLE_RESPONSE_HPP
