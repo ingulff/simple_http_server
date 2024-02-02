344262d59733650342b51f4f42108362b8854e95
Konstantin Gevorkyan <konstantin.gevorkyan@shazzle.com>
2024-02-02 13:59:57 +0500

    Update tests
    Add settings
    Add auth key
    Add generators for keys
    Add tests for single file downloading

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,59 +2,75 @@
  *  ᛝ
  */
 
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
 #include <filesystem>
+#include <string>
 #include <system_error>
 #include <type_traits>
 
-#include <boost/asio/io_context.hpp>
+#include <cstdint>
 
 #include "error/http_error.hpp"
 #include "handle_response.hpp"
 #include "http_client.hpp"
-#include "utils/filepaths.hpp"
+#include "utils/file_generators.hpp"
 #include "utils/filesystem_helpers.hpp"
 
 #include <memory>
 #include "http_session.hpp"
 
-//boost::asio::io_context io_context;
-//auto settings = tt_tests::make_connect({}, {}, {}, {}, {});
+namespace tt_tests
+{
 
-//tt_tests::http_client client( io_context, [](std::string, std::uint32_t) {} );
-//tt_tests::upload_filepaths
-//tt_tests::download_filepaths
+#ifdef HOST_IP
+constexpr std::string_view server_host = SERVER_HOST_IP;
+#else
+constexpr std::string_view server_host = "127.0.0.1";
+#endif // SERVER_HOST_IP
 
+#ifdef PORT_IP
+constexpr std::uint16_t server_port = SERVER_PORT_IP;
+#else
+constexpr std::uint16_t server_port = 1234;
+#endif // SERVER_PORT_IP
+
+
+} // namspace tt_tests
+
+#include "utils/ops.ipp"
 
 TEST_CASE("prepare_files")
 {
 	REQUIRE_FALSE(tt_tests::upload_resource_dir.empty());
 	REQUIRE_FALSE(tt_tests::download_resourse_dir.empty());
+	REQUIRE_FALSE(tt_tests::config_filepath.empty());
 
 	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_exist(tt_tests::upload_resource_dir));
-
 	REQUIRE_NOTHROW(tt_program::utils::throw_if_no_directory(tt_tests::upload_resource_dir));
 	
 	REQUIRE_NOTHROW(tt_tests::generate_upload_filepaths(tt_tests::upload_resource_dir));
-	REQUIRE_NOTHROW(tt_tests::generate_download_filepaths(tt_tests::download_resourse_dir));
+	REQUIRE_NOTHROW(tt_tests::generate_upload_file_hashs(tt_tests::upload_resource_dir));
+	REQUIRE_NOTHROW(tt_tests::generate_uploads_targets_filepaths(tt_tests::upload_resource_dir));
+	//REQUIRE_NOTHROW(tt_tests::generate_download_filepaths(tt_tests::download_resourse_dir));
+	REQUIRE_NOTHROW(tt_tests::generate_config_gates(tt_tests::config_filepath));
+	REQUIRE_NOTHROW(tt_tests::generate_tests_tokens(tt_tests::config_filepath));
 
-	REQUIRE_FALSE(tt_tests::upload_resource_dir.empty());
-
-	REQUIRE_NOTHROW(tt_tests::generate_upload_file_hashs());
+	check_pregenerated_files();
 }
 
 
-TEST_CASE("single_client_session")
+TEST_CASE("single_client_session_without_auth-key")
 {
+	check_pregenerated_files();
+
 	boost::asio::io_context io_context;
 
-	tt_tests::http_client client( io_context, { handle_response_body } );
 #if 0  // есть некоторый конфликт в сертификатах? boost-дефолтные принимает только boost::server
 	SUBCASE("google.com_without_hhtps")
 	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
 		tt_tests::utils::is_reseived = false;
 		tt_tests::utils::is_valid_file = false;
 		tt_tests::utils::expected_errc = error::http_errc::moved_permanently;
@@ -67,6 +83,7 @@ TEST_CASE("single_client_session")
 
 	SUBCASE("google.com_invalid_filepath")
 	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
 		tt_tests::utils::is_reseived = false;
 		tt_tests::utils::is_valid_file = false;
 		tt_tests::utils::expected_errc = error::http_errc::not_found;
@@ -79,10 +96,11 @@ TEST_CASE("single_client_session")
 #endif
 	SUBCASE("single_request_to_http_server_file_no_exist")
 	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
 		tt_tests::utils::is_reseived = false;
 		tt_tests::utils::is_valid_file = false;
-		tt_tests::utils::expected_errc = error::http_errc::not_found;
-		auto settings = tt_tests::make_connect("/qwerty", "127.0.0.1", 1234, 11, tt_tests::http_client::http_method::GET);
+		tt_tests::utils::expected_errc = error::http_errc::unautorized;
+		auto settings = tt_tests::make_connect(std::string{ tt_tests::server_host }, tt_tests::server_port, 0, "/qwerty", 11, tt_tests::http_client::http_method::GET);
 
 		client.send_https( settings );
 
@@ -92,10 +110,47 @@ TEST_CASE("single_client_session")
 
 	SUBCASE("single_request_to_http_server_exist_file")
 	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::unautorized;
+		auto settings = tt_tests::make_connect( std::string{ tt_tests::server_host }, tt_tests::server_port, 0, { tt_tests::upload_target_filepaths[0] }, 11, tt_tests::http_client::http_method::GET);
+
+		client.send_https( settings );
+
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
+}
+
+
+TEST_CASE("single_client_session_with_auth-key")
+{
+	check_pregenerated_files();
+
+	boost::asio::io_context io_context;
+
+	SUBCASE("single_request_to_http_server_file_no_exist")
+	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
+		tt_tests::utils::is_reseived = false;
+		tt_tests::utils::is_valid_file = false;
+		tt_tests::utils::expected_errc = error::http_errc::not_found;
+		auto settings = tt_tests::make_connect(std::string{ tt_tests::server_host }, tt_tests::server_port, *(std::cbegin(tt_tests::auth_tokens)), "/qwerty", 11, tt_tests::http_client::http_method::GET);
+
+		client.send_https( settings );
+
+		io_context.run();
+		CHECK(tt_tests::utils::is_reseived != false);
+	}
+
+	SUBCASE("single_request_to_http_server_exist_file")
+	{
+		tt_tests::http_client client( io_context, { handle_response_body } );
 		tt_tests::utils::is_reseived = false;
 		tt_tests::utils::is_valid_file = true;
 		tt_tests::utils::expected_errc = error::http_errc::successs;
-		auto settings = tt_tests::make_connect("/resource/uploadtest_.txt", "127.0.0.1", 1234, 11, tt_tests::http_client::http_method::GET);
+		auto settings = tt_tests::make_connect( std::string{ tt_tests::server_host }, tt_tests::server_port, *(std::cbegin(tt_tests::auth_tokens)), { tt_tests::upload_target_filepaths[0] }, 11, tt_tests::http_client::http_method::GET);
 
 		client.send_https( settings );
 
@@ -105,7 +160,7 @@ TEST_CASE("single_client_session")
 
 }
 
-
+#if 0
 TEST_CASE("clear_section")
 {
 	REQUIRE_FALSE(tt_tests::download_resourse_dir.empty());
@@ -116,4 +171,4 @@ TEST_CASE("clear_section")
 
 	REQUIRE(tt_tests::clear_downloading_directory() == std::error_code{});
 }
-
+#endif
